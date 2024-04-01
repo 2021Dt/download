4f9870e197485422deb3ab8b0f52f45802aecec4
2021Dt <1538415560@qq.com>
2024-04-01 22:21:00 +0800

    Update DowbLoad.py

--- Tool/DowbLoad.py
+++ Tool/DowbLoad.py
@@ -99,9 +99,3 @@ class DownLoad(object):
             self.download(self.audio_url, '.mp3')
             self.download(self.video_url, '.mp4')
             self.merge_video_and_audio()
-
-
-if __name__ == '__main__':
-    cookie = '''buvid3=FB0CB882-9D22-4B22-B84F-B34D5257A9C435692infoc; b_nut=1710053235; i-wanna-go-back=-1; b_ut=7; _uuid=CFE16EF10-A942-210A2-6577-C15985410B29736137infoc; enable_web_push=DISABLE; buvid4=B7B1128A-02A1-D449-AFC3-FE961957C71036241-024031006-lPdNycUBl7NKGeVq2YrRgQ%3D%3D; rpdid=|(u))uRJ|Jkk0J'u~u||lJkRk; DedeUserID=364386712; DedeUserID__ckMd5=91a731121ca04638; header_theme_version=CLOSE; PVID=1; CURRENT_QUALITY=80; fingerprint=09a311839a24769d1626c4e8f73eac9b; buvid_fp_plain=undefined; FEED_LIVE_VERSION=V_HEADER_LIVE_NO_POP; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTIwNjA4NDksImlhdCI6MTcxMTgwMTU4OSwicGx0IjotMX0.e8b8jECevC7J4e0qCDmX4MJ4964gkULTqNHKlu1BKOw; bili_ticket_expires=1712060789; home_feed_column=5; browser_resolution=1844-1059; CURRENT_BLACKGAP=0; CURRENT_FNVAL=4048; bp_video_offset_364386712=914693820644327462; SESSDATA=3e5b1cf2%2C1727362996%2C3c944%2A31CjA7CV0DOedMGxZbA--wZVsocnFkLRFEMlKMl1MOB_KBwpnbGKh3UVs1IeHtbHa8qTcSVlNjb1E5dlZKMlFLYzRUZ1FNalR4YzhySmt6am5ETExqcmNNQ21xYnNuVVVDVGt2bzVIOG1fMTdsUDlJazZPMUV6UVk3VU5PNmdFTDJwbl9FYjhrcnFBIIEC; bili_jct=955033a1cdb68d777532c9c5c683a655; buvid_fp=09a311839a24769d1626c4e8f73eac9b; b_lsid=DF5D10BFC_18E92A1044E; sid=nlnadpez'''
-    url = 'https://www.bilibili.com/festival/bnj2024?bvid=BV1iU421d7aG&spm_id_from=333.337.top_right_bar_window_history.content.click'
-    DownLoad(url, cookie=cookie).run()
